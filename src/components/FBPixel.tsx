736342628c8564bf5f6ac6718fa1b8ab55adf020
t99sultan-cmyk <timur@MacBook-Pro-Admin-4.local>
2026-04-10 23:40:15 +0700

    fix: update FB pixel ID to new account

--- src/components/FBPixel.tsx
+++ src/components/FBPixel.tsx
@@ -38,7 +38,7 @@ export function FBPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+            fbq('init', '1392840799194187');
             fbq('track', 'PageView');
           `,
         }}
@@ -48,7 +48,7 @@ export function FBPixel() {
           height="1"
           width="1"
           style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          src="https://www.facebook.com/tr?id=1392840799194187&ev=PageView&noscript=1"
         />
       </noscript>
     </>
